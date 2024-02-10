033b06a626317ca2d5617bbeda9328b397aff6d0
Pavlo Karas <kar.pavel123@gmail.com>
2024-02-10 17:28:52 +0200

    feat: create daf function

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -26,7 +26,6 @@ function Dif()
 
     -- Get the node at the cursor position
     local node = ts.get_node_at_cursor()
-    print(node)
 
     -- Find the nearest ancestor function node
     while node do
@@ -38,7 +37,7 @@ function Dif()
 
     -- If a function node is found, delete it
     if node then
-        local start_row, start_col, end_row, end_col = node:range()
+        local start_row, _, end_row, _ = node:range()
         vim.api.nvim_buf_set_lines(bufnr, start_row, end_row + 1, false, {})
     else
         print('No function found under cursor')
@@ -46,4 +45,47 @@ function Dif()
 end
 
 vim.api.nvim_set_keymap('n', 'dif', ':lua Dif()<CR>', { noremap = true, silent = true })
+
+function Daf()
+    local ts = require('nvim-treesitter.ts_utils')
+
+    -- Get the current buffer
+    local bufnr = vim.api.nvim_get_current_buf()
+
+    -- Get the node at the cursor position
+    local node = ts.get_node_at_cursor()
+
+
+    -- Find the nearest ancestor function node
+    while node do
+        if node:type() == 'function_declaration' or node:type() == 'arrow_function' then
+            break
+        end
+        node = node:parent()
+    end
+
+
+    if node then
+        local delete_start, _, delete_end, _ = node:range()
+        local prev_node = ts.get_previous_node(node, true, true);
+        local next_node = ts.get_next_node(node, true, true);
+
+        if prev_node then
+          local _, _, prev_node_end, _ = prev_node:range();
+          delete_start = prev_node_end + 1;
+        end
+
+        if next_node then
+          local next_node_start, _, _, _ = next_node:range();
+          delete_end = next_node_start - 2;
+        end
+
+        print(delete_start, delete_end);
+        vim.api.nvim_buf_set_lines(bufnr, delete_start, delete_end + 1, false, {})
+    else
+        print('No function found under cursor')
+    end
+end
+
+vim.api.nvim_set_keymap('n', 'daf', ':lua Daf()<CR>', { noremap = true, silent = true })
 

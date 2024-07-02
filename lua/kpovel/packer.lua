4b007b1408697f64d507ce7dbe124ace7815e969
Pavlo Karas <thepavlokaras@gmail.com>
2024-07-02 05:28:41 +0300

    remove trouble

--- lua/kpovel/packer.lua
+++ lua/kpovel/packer.lua
@@ -17,18 +17,6 @@ return require("packer").startup(function(use)
     as = "tokyonight",
   })
 
-  use({
-    "folke/trouble.nvim",
-    config = function()
-      require("trouble").setup {
-        icons = false,
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
-      }
-    end
-  })
-
   use {
     'nvim-treesitter/nvim-treesitter',
     run = function()

5d92ab2451589bb8c75c50ff2054fd14fa2aa91e
Pavlo Karas <thepavlokaras@gmail.com>
2024-12-16 15:00:04 +0200

    quickfix

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,4 +1,6 @@
 local builtin = require('telescope.builtin')
+local telescope_actions = require "telescope.actions"
+
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
@@ -9,3 +11,14 @@ vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
 vim.keymap.set('n', 'td', builtin.lsp_type_definitions, {})
 vim.keymap.set('n', '<leader>wd', builtin.diagnostics, {})
 vim.keymap.set('n', '<leader>ws', builtin.lsp_dynamic_workspace_symbols, {})
+
+
+require('telescope').setup {
+  defaults = {
+    mappings = {
+      i = {
+        ["<C-s>"] = telescope_actions.send_selected_to_qflist + telescope_actions.open_qflist
+      }
+    }
+  }
+}

37df1e7c8c57d53e0bd7ca7ee51c507b49aef7b9
Pavlo Karas <thepavlokaras@gmail.com>
2025-02-09 09:40:20 +0200

    fix: delete junk tailwind root patterns

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -30,13 +30,8 @@ lspconfig.tailwindcss.setup {
     'tailwind.config.cjs',
     'tailwind.config.mjs',
     'tailwind.config.ts',
-    'postcss.config.js',
-    'postcss.config.cjs',
-    'postcss.config.mjs',
-    'postcss.config.ts',
     'package.json',
-    'node_modules',
-    '.git'
+    'node_modules'
   )
 }
 

20f0e422242effbff28a1d5d5f27cbb4ef2a7b65
Pavlo Karas <thepavlokaras@gmail.com>
2025-08-03 22:10:56 +0300

    it works, but I don't like it

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,24 +1,21 @@
 vim.api.nvim_create_autocmd("LspAttach", {
-	callback = function(ev)
-		local client = vim.lsp.get_client_by_id(ev.data.client_id)
-		if client:supports_method("textDocument/completion") then
-			vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
-		end
-	end,
+    callback = function(ev)
+        local client = vim.lsp.get_client_by_id(ev.data.client_id)
+        if client:supports_method("textDocument/completion") then
+            vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+
+            local opts = { buffer = bufnr, silent = true }
+
+            vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+            vim.keymap.set("i", "<C-Space>", vim.lsp.completion.get, opts)
+            vim.keymap.set({"i", "n"}, "<C-h>", vim.lsp.buf.signature_help, opts)
+        end
+
+    end,
 })
 
 vim.cmd("set completeopt+=noselect")
 
-
-require('lspconfig').elixirls.setup({
-      cmd = { vim.fn.expand("~/.local/share/nvim/mason/packages/elixir-ls/language_server.sh") }
-})
-
-require("mason-lspconfig").setup({ })
-
-vim.lsp.enable({
-"elixir-ls",
-"html-lsp",
-"tailwindcss-language-server",
-"typescript-language-server",
+vim.diagnostic.config({
+  virtual_text = true
 })

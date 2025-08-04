b3f6ee04ac02c09ed24a84cb44cde14a9aa93122
Pavlo Karas <thepavlokaras@gmail.com>
2025-08-04 19:55:29 +0300

    Apparently, I didn't need that weird function

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -6,7 +6,6 @@ vim.pack.add({
 	{src = "https://github.com/nvim-telescope/telescope.nvim", tag = "0.1.8"},
 	{src = "https://github.com/nvim-treesitter/nvim-treesitter", branch = "master" },
   {src = "https://github.com/mason-org/mason.nvim" },
-  {src = "https://github.com/neovim/nvim-lspconfig" }
-	-- mason,
-	-- lsp
+  {src = "https://github.com/mason-org/mason-lspconfig.nvim" },
+  {src = "https://github.com/neovim/nvim-lspconfig" },
 })

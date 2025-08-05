73a17f36f42cbd6ffa3718b4c947f858a1a61c26
Pavlo Karas <thepavlokaras@gmail.com>
2025-08-05 11:42:20 +0300

    treesitter for liveview

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,5 +1,5 @@
-require('nvim-treesitter.configs').setup({
-  ensure_installed = { },
+require("nvim-treesitter.configs").setup({
+  ensure_installed = {"eex", "elixir", "erlang", "heex", "html", "surface", "diff"},
   sync_install = false,
   auto_install = true,
 

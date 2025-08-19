53ab52d936d32816c20ebc06cec57111117d19b7
Pavlo Karas <thepavlokaras@gmail.com>
2025-08-19 15:46:15 +0300

    rookie mistake

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -8,4 +8,5 @@ vim.pack.add({
   {src = "https://github.com/mason-org/mason.nvim" },
   {src = "https://github.com/mason-org/mason-lspconfig.nvim" },
   {src = "https://github.com/neovim/nvim-lspconfig" },
+  {src = "https://github.com/mbbill/undotree" },
 })

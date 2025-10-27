c4ce08564f28927c07aa20f2a0b463d427779e53
Pavlo Karas <thepavlokaras@gmail.com>
2025-10-27 10:52:19 +0200

    ts

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -10,7 +10,7 @@ require("nvim-treesitter.configs").setup({
       local max_filesize = 200 * 1024 -- 100 KB
       local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
       if ok and stats and stats.size > max_filesize then
-        return true
+        -- return true
       end
     end,
 

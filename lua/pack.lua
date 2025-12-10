bb77b49b4acb2bdf5f7e73603c08f107bfb2db66
Pavlo Karas <thepavlokaras@gmail.com>
2025-12-11 00:30:32 +0200

    update telescope

--- lua/pack.lua
+++ lua/pack.lua
@@ -3,7 +3,7 @@ vim.pack.add({
 	{src = "https://github.com/ThePrimeagen/harpoon"},
 	{src = "https://github.com/laytan/cloak.nvim"},
 	{src = "https://github.com/nvim-lua/plenary.nvim"},
-	{src = "https://github.com/nvim-telescope/telescope.nvim", tag = "0.1.8"},
+	{src = "https://github.com/nvim-telescope/telescope.nvim", tag = "0.2.0"},
 	{src = "https://github.com/nvim-treesitter/nvim-treesitter", branch = "master" },
   {src = "https://github.com/mason-org/mason.nvim" },
   {src = "https://github.com/mason-org/mason-lspconfig.nvim" },
